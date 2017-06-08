e3c00e90748a211ddffe908e1c224d55c01daa6f
Neel Basu <neel.basu.z@gmail.com>
2017-06-08 14:03:10 +0530

    - now highlights story-viewer if story is already open

--- identity/static/js/sprite/story-viewer.js
+++ identity/static/js/sprite/story-viewer.js
@@ -1,20 +1,36 @@
 $(document).ready(function(){
     $(document).on('click', "div.story-viewer-close", function(event){
         $(this).parent().hide();
+        $(this).parent().remove();
     });
     $(document).on('click', "div.story-viewer .ministory-linked", function(event){
         var refered_id = $($(this).find('.ministory-sprite')[0]).data('id');
         // $('.story-viewer').remove();
-        var id = refered_id;
-        $.ajax({
-            url: '/_story/'+id
-        }).done(function(content, status, xhr){
-            var content_length = content.length;
-            if(content_length){
-                var story_board = $($.trim(content.replace(/\s+/g, " ")));
-                $('#main').append(story_board);
-                story_board.css('left', story_board.outerWidth()*($('.story-viewer').length-1));
+        var story_already_open = false;
+        $('.story-viewer').each(function(){
+            var story_id = $(this).data('story');
+            if(story_id == refered_id){
+                story_already_open = true;
+                var prestate = $(this).css('border');
+                $(this).css('border', '2px solid yellow');
+                var self = $(this);
+                setTimeout(function(){
+                    self.css('border', prestate);
+                }, 1000);
             }
-        });
+        })
+        if(!story_already_open){
+            var id = refered_id;
+            $.ajax({
+                url: '/_story/'+id
+            }).done(function(content, status, xhr){
+                var content_length = content.length;
+                if(content_length){
+                    var story_board = $($.trim(content.replace(/\s+/g, " ")));
+                    $('#main').append(story_board);
+                    story_board.css('left', story_board.outerWidth()*($('.story-viewer').length-1));
+                }
+            });
+        }
     })
 });
