ff8246a91a1f008ebd8d6f402307e2ac8ffa4a9a
Neel Basu <neel.basu.z@gmail.com>
2017-09-06 19:51:15 +0530

    - removing unnecessary print statement

--- identity/models.py
+++ identity/models.py
@@ -337,7 +337,6 @@ class Story(models.Model):
             except ValueError:
                 raise ValidationError('Malformed prescription data')
         if self.media and len(self.media) > 0:
-            print(self.media)
             try:
                 media = self.media
                 for m in media:
